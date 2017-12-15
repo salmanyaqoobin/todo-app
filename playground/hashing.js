edceb2f070715cb75ad8b312dfa7dceb65e408b1
Salman Yaqoob <sy@in-hq.com>
2017-12-16 02:26:46 +0300

    Add hash password before saving to database

--- playground/hashing.js
+++ playground/hashing.js
@@ -5,18 +5,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs');
+
+var password  = "salman123";
+
+//bcrypt.genSalt(10, function(err, salt) {
+//    bcrypt.hash(password, salt, function(err, hash) {
+//        console.log(hash);
+//    });
+//});
+
+var hashedPassword = "$2a$10$NMCProMX4uLlza0y1W2Lren.i0Z9ced.qe18FO8dk64.pSlhAunqW";
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+    console.log(res);
+});
+
+
 //jwt.sign (Assign token)
 //jwt.verify (Verify token)
 
-var data = {
-    id: 4
-};
-
-var token = jwt.sign(data, 'abc123');
-console.log('token:', token);
-
-let decoded = jwt.verify(token, 'abc123');
-console.log('decoded: ', decoded);
+//var data = {
+//    id: 4
+//};
+//
+//var token = jwt.sign(data, 'abc123');
+//console.log('token:', token);
+//
+//let decoded = jwt.verify(token, 'abc123');
+//console.log('decoded: ', decoded);
 
 //var message = 'i am user number 3';
 //var hash = SHA256(message).toString();
